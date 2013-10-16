e7bc55a0eb074b80340277297b16ed9fb57c076d
Peter van den Berg <Peter_vd_berg@hotmail.com>
2013-10-16 16:50:15 +0200

    front page message change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-	@messages = Message.order('created_at DESC').last(2)
+	@messages = Message.order('created_at DESC').first(2)
 	@event = Event.where('start_at >= ?', Date.today).order("start_at ASC").first
 	
 	@there = @event.attendances.where(attending: true)
